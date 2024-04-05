8b220fcfa79336a72c770deb2e4faa42b42bec0d
kren333 <90985278+kren333@users.noreply.github.com>
2024-04-04 22:38:05 -0400

    api key -> env variable; updating translate function to have failure messages; removing deprecated comments

--- src/translator.py
+++ src/translator.py
@@ -1,16 +1,19 @@
 import pathlib
 import textwrap
+import os
+from tqdm import tqdm
 
 import google.generativeai as genai
 
 from IPython.display import display
 from IPython.display import Markdown
+import pdb
 
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-genai.configure(api_key='AIzaSyCfh7jdcD6VMJEjiG7eOttexDsbHzS3b2o')
+genai.configure(api_key=os.environ['API_KEY']) # the api key should be saved as an env var via export API_KEY = ...
 
 model = genai.GenerativeModel('gemini-pro')
 
@@ -19,82 +22,35 @@ def get_translation(post: str) -> str:
     return response.text
 
 def get_language(post: str) -> str:
-    response = model.generate_content("What language is this? If it is gibberish, please say 'Malformed': " + post)
+    response = model.generate_content("What language is this? If it is not written in a recognizable language, please say 'Impossible': " + post)
     return response.text
 
 def translate_content(post: str) -> tuple[bool, str]:
-  # use zero-shot prompting, due to fairly strong empirical results
+  failure_text = "Sorry - we failed to properly parse and translate your post! Something may be wrong with our backend :/"
 
   classified_language = get_language(post)
+  # if something went wrong with classification, account for this
+  error_keywords = ["understand", "didn't", "sorry"]
+  for word in error_keywords:
+    if word in classified_language:
+      return (False, failure_text)
+  if not classified_language or not isinstance(classified_language, str):
+    return (False, failure_text)
+
   # if the post is in english to begin with, return the original content
   if "english" in classified_language.lower():
     return (True, post)
   # if the post seems malformed, make this known
-  if "malformed" in classified_language.lower():
+  if "impossible" in classified_language.lower():
     return (False, "Malformed")
   # otherwise, translate the sentence to english, and return
   translated = get_translation(post)
+
+  # if something went wrong with translation, account for this
+  if not translated or not isinstance(translated, str):
+    return (False, failure_text)
+
   return (False, translated)
-
-
-# from typing import Callable
-
-# from google.colab import auth
-# from google.cloud import aiplatform
-
-# PROJECT_ID = "nodebb-416919" # TODO: Replace with your GCP Project ID
-
-# # Set the project id
-# ! gcloud config set project {PROJECT_ID}
-
-# auth.authenticate_user()
-
-# aiplatform.init(
-#     # your Google Cloud Project ID or number
-#     # environment default used is not set
-#     project=PROJECT_ID,
-
-#     # the Vertex AI region you will use
-#     # defaults to us-central1
-#     location='us-central1',
-# )
-
-# from vertexai.language_models import ChatModel, InputOutputTextPair
-
-# chat_model = ChatModel.from_pretrained("chat-bison@001")
-
-# def get_translation(post: str) -> str:
-#     parameters = {
-#         "temperature": 0.7,  # Temperature controls the degree of randomness in token selection.
-#         "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
-#     }
-
-#     chat = chat_model.start_chat(context=post)
-#     response = chat.send_message(post, **parameters)
-#     return response.text
-
-# def get_language(post: str) -> str:
-#     parameters = {
-#         "temperature": 0.7,  # Temperature controls the degree of randomness in token selection.
-#         "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
-#     }
-#     chat = chat_model.start_chat(context=post)
-#     response = chat.send_message(post, **parameters)
-#     return response.text
-
-# def query_llm(post: str) -> tuple[bool, str]:
-#   # use zero-shot prompting, due to fairly strong empirical results
-
-#   classified_language = get_language(post)
-#   # if the post is in english to begin with, return the original content
-#   if "english" in classified_language.lower():
-#     return (True, post)
-#   # if the post seems malformed, make this known
-#   if "malformed" in classified_language.lower():
-#     return (False, "Malformed")
-#   # otherwise, translate the sentence to english, and return
-#   translated = get_translation(post)
-#   return (False, translated)
 
 
 
